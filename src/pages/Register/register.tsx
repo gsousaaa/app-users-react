92c008edccae7837fd2fbf232749a7b05347cce4
gsousaaa <glaucoezequiel94@gmail.com>
2025-06-23 16:43:36 -0300

    fix: handleRegister

--- src/pages/Register/register.tsx
+++ src/pages/Register/register.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { useState } from 'react'
 import { useAuth } from '../../contexts/AuthContext.tsx'
-import {  registerUser } from '../../clients/api.ts'
+import { registerUser } from '../../clients/api.ts'
 import { RegisterForm } from '../../components/auth/RegisterForm.tsx'
 import type { RegisterType } from '../../schemas/registerSchema.ts'
 
@@ -11,7 +11,7 @@ const Register = () => {
     const { login, user } = useAuth()
     const [loading, setLoading] = useState(false)
 
-    const handleLogin = async (data: RegisterType) => {
+    const handleRegister = async (data: RegisterType) => {
         setLoading(true)
 
         try {
@@ -26,23 +26,24 @@ const Register = () => {
 
             login(token) // salva no contexto e cookies
 
-            if (user?.role === 'user') {
-                toast.success(`Cadastro realizado com sucesso!, seja bem-vindo, ${user?.name}`)
-                navigate('/profile')
-            }
+
         } catch (error: any) {
             toast.error(
                 error.message || error.message[0] || 'Erro ao se cadastrar. Tente novamente mais tarde.'
             )
         } finally {
             setLoading(false)
+            if (user?.role === 'user') {
+                toast.success(`Cadastro realizado com sucesso!, seja bem-vindo, ${user?.name}`)
+                navigate('/profile')
+            }
         }
     }
 
     return (
         <div className="flex items-center justify-center h-screen bg-gray-100">
             <div className="p-10 rounded w-[90%] max-w-md">
-                <RegisterForm onRegister={handleLogin} />
+                <RegisterForm onRegister={handleRegister} />
             </div>
         </div>
     )
