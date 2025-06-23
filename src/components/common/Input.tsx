e984ab782efd22f7430eeb8f28dda0c769763964
gsousaaa <glaucoezequiel94@gmail.com>
2025-06-23 03:31:24 -0300

    feat: Input component

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,15 @@
+interface InputProps {
+    type: string,
+    placeholder: string,
+    value: string,
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    name: string,
+    disabled?: boolean
+
+}
+
+export const Input: React.FC<InputProps> = ({ type, disabled, placeholder, value, onChange, name }) => {
+    return (
+        <input disabled={disabled} className='text-sm text-black w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-gray-100 font-semibold' type={type} placeholder={placeholder} value={value} onChange={onChange} name={name} />
+    )
+}
