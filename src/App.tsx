88ccdf755609eea1f9e96261ed16a916acb23238
gsousaaa <glaucoezequiel94@gmail.com>
2025-06-23 13:29:16 -0300

    feat: using private routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login/login'
 import Profile from './pages/Profile/profile'
+import { AdminUsers } from './pages/AdminDashboard/adminDashboard'
+import PrivateRoute from './routes/PrivateRoute'
+import AdminRoute from './routes/AdminRoute'
+import Register from './pages/Register/register'
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/profile" element={<Profile />} />
+
+      <Route path="/register" element={<Register />} />
+
+      <Route element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+
+        <Route element={<AdminRoute />}>
+          <Route path="/admin/users" element={<AdminUsers />} />
+        </Route>
+      </Route>
     </Routes>
   )
 }
